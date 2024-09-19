9a771731b0d360968cbb36640d7df448e3906a2a
Marta Nogueira <martoocas.fn@gmail.com>
2024-09-19 11:01:03 +0100

    Created base structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Portfolio</header>
+      <div className="container">
+        <header>Marta Nogueira</header>
+        <div className="row">
+          <div className="col-3">Photo</div>
+          <div className="col">About</div>
+        </div>
+        <div className="row">
+          <div className="col">Projects</div>
+          <div className="col-4">Skills</div>
+        </div>
+        <footer>Contacts</footer>
+      </div>
     </div>
   );
 }
